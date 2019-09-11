be37901ae5ff57ba7c9ad1ed0029bd5855b9d25d
Natalia <nataliaandreica@yahoo.com>
2019-09-11 20:55:43 +0300

    first

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String name = "Popescu";
+        String firstname = "Ion";
+        byte age = 99;
+        int phoneNumber = 0755344997
+        char gender = 'M';
+
+        System.out.println("Contact details ");
+        System.out.println(name + " " + firstname\n"Age" + age\n"Phone Number" + phoneNumber);
+    }
+
+
+}
