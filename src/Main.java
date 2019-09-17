91990c2ace6e8f95b80f09ee2a581cc372941b39
Natalia <nataliaandreica@yahoo.com>
2019-09-17 21:28:44 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -12,24 +12,23 @@ public class Main {
         byte d = 8;
         byte f = 6;
         int multiple = d * f;
-        int sum =-c + multiple;
+        int sum = -c + multiple;
         System.out.println("a." + sum);
         byte g = 55;
         byte h = 9;
         int sum2 = g + h;
         float average = sum2 / 9f;
         System.out.println("b." + average);
-    byte i = 20;
-byte j = 8;
-int multiple2 = -b*c;
-float divide = multiple2/8f;
-float sum3 = i + divide;
+        byte i = 20;
+        byte j = 8;
+        int multiple2 = -b * c;
+        float divide = multiple2 / 8f;
+        float sum3 = i + divide;
         System.out.println("c." + sum3);
-float x = 15/3;
-float y = x*2;
-float z = 8/3f;
-float w = (c+y)-z;
+        float x = 15 / 3;
+        float y = x * 2;
+        float z = 8 / 3f;
+        float w = (c + y) - z;
         System.out.println("d." + w);
-
-
-}}
+    }
+}
