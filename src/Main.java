cb82f73f2b3d82003f1bc7f4ec648dd78c55a5a0
Natalia <nataliaandreica@yahoo.com>
2019-09-17 21:22:07 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        String name = "Popescu";
-        String firstname = "Ion";
-        byte age = 99;
-        int phoneNumber = 0755344997
-        char gender = 'M';
+        System.out.println("Hello");
+        String name = "Natalia";
+        byte a = 10;
+        byte b = 3;
+        System.out.println(name);
+        System.out.println(a + b);
+        System.out.println(10.0 / 3);
+        byte c = 5;
+        byte d = 8;
+        byte f = 6;
+        int multiple = d * f;
+        int sum =-c + multiple;
+        System.out.println("a." + sum);
+        byte g = 55;
+        byte h = 9;
+        int sum2 = g + h;
+        float average = sum2 / 9f;
+        System.out.println("b." + average);
+    byte i = 20;
+byte j = 8;
+int multiple2 = -b*c;
+float devide = multiple2/8f;
+float sum3 = i + devide;
+        System.out.println("c." + sum3);
+float x = 15/3;
+float y = x*2;
+float z = 8/3f;
+float w = (c+y)-z;
+        System.out.println("d." + w);
 
-        System.out.println("Contact details ");
-        System.out.println(name + " " + firstname\n"Age" + age\n"Phone Number" + phoneNumber);
-    }
 
-
-}
+}}
