a78ac992891db63a0d58477fa3cb76f14530f1f4
Natalia <nataliaandreica@yahoo.com>
2019-09-18 20:56:17 +0300

    first

---
+++ src/lab2/Main.java
@@ -0,0 +1,38 @@
+package lab2;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int x = 3;
+        int y = 5;
+        int result = sum(x, y);
+        System.out.println(result);
+        result = substract(x, y);
+        System.out.println(result);
+        result = multiply(x, y);
+        System.out.println(result);
+        float resultdiv = divide(x, y);
+        System.out.println(resultdiv);
+
+    }
+
+    public static int sum(int first, int second) {
+        int rez = first + second;
+        return rez;
+    }
+
+    public static int substract(int first, int second) {
+        int rez = first - second;
+        return rez;
+    }
+
+    public static int multiply(int first, int second) {
+        int rez = first * second;
+        return rez;
+    }
+
+    public static float divide(float first, float second) {
+        return first / second;
+
+    }
+}
