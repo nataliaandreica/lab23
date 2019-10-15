d51dc4008d0c5d1a8ee8d8dcdad77cc6c0abfb6e
Natalia <nataliaandreica@yahoo.com>
2019-10-15 21:01:15 +0300

    first

---
+++ src/lab3/Calculator/LogicalOp.java
@@ -0,0 +1,11 @@
+package lab3.Calculator;
+
+public class LogicalOp {
+    public int verifyGreaterNumber(int first, int second) {
+        if (first > second) {
+            return first;
+        } else {
+            return second;
+        }
+    }
+}
---
+++ src/lab3/Calculator/Main.java
@@ -0,0 +1,10 @@
+package lab3.Calculator;
+
+public class Main{
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 5;
+LogicalOp op = new LogicalOp();
+op.verifyGreaterNumber(x,y);
+    }
+}
